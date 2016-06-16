aae362f601c0182c5de64c5deaa92f3783506284
alkis <a.kafkis@gmail.com>
2016-06-16 16:10:17 +0300

    init commit

---
+++ app/src/main/java/gr/crystalogic/commons/entities/BaseObject.java
@@ -0,0 +1,16 @@
+package gr.crystalogic.commons.entities;
+
+public class BaseObject {
+
+    public static final String COL_ID = "_id";
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
