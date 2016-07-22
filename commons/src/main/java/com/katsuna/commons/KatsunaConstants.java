3fe56a371cccf5b237eff9241dc3d08b6195c357
alkis <a.kafkis@gmail.com>
2016-07-22 15:58:59 +0300

    settings manager

---
+++ commons/src/main/java/com/katsuna/commons/KatsunaConstants.java
@@ -0,0 +1,7 @@
+package com.katsuna.commons;
+
+public class KatsunaConstants {
+
+    public static final String PROFILE_KEY = "profile";
+
+}
---
+++ commons/src/main/java/com/katsuna/commons/utils/SettingsManager.java
@@ -0,0 +1,33 @@
+package com.katsuna.commons.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SettingsManager {
+
+    public static void setSetting(Context context, String key, int value) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    public static int readSetting(Context context, String key, int defaultValue) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        return settings.getInt(key, defaultValue);
+    }
+
+    public static void setSetting(Context context, String key, String value) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String readSetting(Context context, String key, String defaultValue) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        return settings.getString(key, defaultValue);
+    }
+
+}
