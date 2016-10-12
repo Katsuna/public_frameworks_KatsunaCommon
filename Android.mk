b71de4a3e6523621b0c1e3815dbb476cb304522e
George G <kreach3r@users.noreply.github.com>
2016-10-12 12:09:53 +0200

    picasso isn't needed in Common
    But it is needed in all other apps, so it joins roundedimageview in the
    "defined but not used" category.
    
    This commit also needs commits in all other apps, where the local static library
    will be specified for inclusion.

--- Android.mk
+++ Android.mk
@@ -2,8 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # Add here only the libs (.jar) that the KatsunaCommon itself needs, to be built
-LOCAL_STATIC_JAVA_LIBRARIES := picasso
-LOCAL_STATIC_JAVA_LIBRARIES += jodatime
+LOCAL_STATIC_JAVA_LIBRARIES := jodatime
 
 LOCAL_MODULE := KatsunaCommon
 LOCAL_CERTIFICATE := platform
@@ -14,11 +13,11 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := picasso:aosp/libs/picasso-2.5.2.jar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += jodatime:aosp/libs/jodatime-2.9.2.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := jodatime:aosp/libs/jodatime-2.9.2.jar
 
 # This is a definition!
 # Add here all the libs (jar, aar) that are needed in all Katsuna Apps but not necessarily in KatsunaCommon
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += picasso:aosp/libs/picasso-2.5.2.jar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += roundedimageview:aosp/libs/roundedimageview-2.2.1.aar
 
 include $(BUILD_MULTI_PREBUILT)
