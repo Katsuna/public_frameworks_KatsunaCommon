951b93aaaa38d151f38a70f216b22bb03f22205f
George G <kreach3r@users.noreply.github.com>
2016-10-12 11:47:40 +0200

    Android.mk: Use 'aosp/libs' instead of 'commons/libs'
    This way, there are no conflicts with build.gradle

--- Android.mk
+++ Android.mk
@@ -14,11 +14,11 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := picasso:commons/libs/picasso-2.5.2.jar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += jodatime:commons/libs/jodatime-2.9.2.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := picasso:aosp/libs/picasso-2.5.2.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += jodatime:aosp/libs/jodatime-2.9.2.jar
 
 # This is a definition!
 # Add here all the libs (jar, aar) that are needed in all Katsuna Apps but not necessarily in KatsunaCommon
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += roundedimageview:commons/libs/roundedimageview-2.2.1.aar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += roundedimageview:aosp/libs/roundedimageview-2.2.1.aar
 
 include $(BUILD_MULTI_PREBUILT)
