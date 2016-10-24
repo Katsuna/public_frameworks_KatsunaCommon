6d76c4f67763b98968eaf25f9cc4c5c573bf61c8
alkis <a.kafkis@gmail.com>
2016-10-24 19:38:55 +0300

    retrieving launcher access logs per app

--- commons/src/main/java/com/katsuna/commons/utils/LauncherAccessReader.java
+++ commons/src/main/java/com/katsuna/commons/utils/LauncherAccessReader.java
@@ -10,6 +10,7 @@ import com.katsuna.commons.providers.LauncherProvider;
 import org.joda.time.DateTime;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LauncherAccessReader {
@@ -19,18 +20,42 @@ public class LauncherAccessReader {
         Cursor cursor = context.getContentResolver().query(LauncherProvider.URI_LAUNCHER_ACCESS,
                 null, null, null, null);
 
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                LauncherAccess lAccess = new LauncherAccess();
-                lAccess.setId(cursor.getLong(cursor.getColumnIndex(LauncherAccess.COL_ID)));
-                lAccess.setComponent(cursor.getString(cursor.getColumnIndex(LauncherAccess.COL_COMPONENT)));
-                lAccess.setUser(cursor.getString(cursor.getColumnIndex(LauncherAccess.COL_USER)));
-                lAccess.setTime(cursor.getString(cursor.getColumnIndex(LauncherAccess.COL_TIME)));
-                lAccesses.add(lAccess);
-            } while (cursor.moveToNext());
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    LauncherAccess lAccess = new LauncherAccess();
+                    lAccess.setId(cursor.getLong(cursor.getColumnIndex(LauncherAccess.COL_ID)));
+                    lAccess.setComponent(cursor.getString(cursor.getColumnIndex(LauncherAccess.COL_COMPONENT)));
+                    lAccess.setUser(cursor.getString(cursor.getColumnIndex(LauncherAccess.COL_USER)));
+                    lAccess.setTime(cursor.getString(cursor.getColumnIndex(LauncherAccess.COL_TIME)));
+                    lAccesses.add(lAccess);
+                } while (cursor.moveToNext());
+            }
             cursor.close();
         }
         return lAccesses;
+    }
+
+    public static HashMap<String, Integer> getLauncherStatsPerApp(Context context) {
+        HashMap<String, Integer> output = new HashMap<>();
+        String[] projection = new String[]{"count(" + LauncherAccess.COL_USER + ") as launch_count", LauncherAccess.COL_USER};
+        String selection = "(0 == 0) GROUP BY (" + LauncherAccess.COL_USER + ")";
+
+        Cursor cursor = context.getContentResolver().query(LauncherProvider.URI_LAUNCHER_ACCESS, projection, selection, null,
+                null);
+
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    int count = cursor.getInt(0);
+                    String intentUri = cursor.getString(1);
+                    output.put(intentUri, count);
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
+
+        return output;
     }
 
     public static void save(Context context, LauncherAccess lAccess) {
