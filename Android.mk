a6d0e04b41c8209db2593a764d1e994af3580f49
George G <kreach3r@users.noreply.github.com>
2017-01-30 21:41:03 +0100

    Android.mk fix build after latest changes
    Added needed import
    Added needed dependency (now defined in KatsunaCommon but bundled in here)

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_STATIC_JAVA_LIBRARIES := jodatime
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
 
 LOCAL_MODULE := KatsunaCommon
@@ -22,5 +23,6 @@ LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := jodatime:aosp/libs/jodatime-2.9.2.jar
 # Add here all the libs (jar, aar) that are needed in all Katsuna Apps but not necessarily in KatsunaCommon
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += picasso:aosp/libs/picasso-2.5.2.jar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += roundedimageview:aosp/libs/roundedimageview-2.2.1.aar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += fabtransformation:aosp/libs/fabtransformation-1.0.0.aar
 
 include $(BUILD_MULTI_PREBUILT)
