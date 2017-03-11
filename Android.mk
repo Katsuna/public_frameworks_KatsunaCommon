4c341f0a19e4fa72074726dac95050931259e974
George G <kreach3r@users.noreply.github.com>
2017-03-11 12:45:12 +0100

    Include roundedimageview for AOSP compilation
    Fixes included

--- Android.mk
+++ Android.mk
@@ -15,12 +15,17 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
 
+LOCAL_STATIC_JAVA_AAR_LIBRARIES := roundedimageview
+
 LOCAL_MODULE := KatsunaCommon
 
 LOCAL_SDK_VERSION := $(SUPPORT_CURRENT_SDK_VERSION)
 
-LOCAL_USE_AAPT2 := true
-LOCAL_AAPT_FLAGS := --no-version-vectors
+#LOCAL_USE_AAPT2 := true
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --generate-dependencies
+LOCAL_AAPT_FLAGS += --no-version-vectors
 LOCAL_JAR_EXCLUDE_FILES := none
 #LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 
