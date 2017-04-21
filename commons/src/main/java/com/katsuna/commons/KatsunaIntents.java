1c8ddbb0e298e9690def50fa6d4a48f0ca4b4d0f
alkis <a.kafkis@gmail.com>
2017-04-21 20:26:48 +0300

    Missed call notification opens Katsuna Calls app.

---
+++ commons/src/main/java/com/katsuna/commons/KatsunaIntents.java
@@ -0,0 +1,9 @@
+package com.katsuna.commons;
+
+/**
+ * Helper class to store intents related info.
+ */
+
+public class KatsunaIntents {
+    public static final String ACTION_CALLS_VIEW = "com.katsuna.calls.view";
+}
