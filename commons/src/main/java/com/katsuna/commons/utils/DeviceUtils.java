edc4b6283660870abc62ee545c9f239c04559b97
alkis <a.kafkis@gmail.com>
2017-06-27 17:37:43 +0300

    Device Provision check.

--- commons/src/main/java/com/katsuna/commons/utils/DeviceUtils.java
+++ commons/src/main/java/com/katsuna/commons/utils/DeviceUtils.java
@@ -1,6 +1,8 @@
 package com.katsuna.commons.utils;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
@@ -18,4 +20,10 @@ public class DeviceUtils {
     public static int getDisplayHeight(Context context) {
         return getDisplayMetrics(context).heightPixels;
     }
+
+    public static boolean isUserSetupComplete(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        return Settings.Secure.getInt(resolver, Settings.Global.DEVICE_PROVISIONED, 0) != 0;
+    }
+
 }
