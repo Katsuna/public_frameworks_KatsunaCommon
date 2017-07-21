8e298dd29030bf0ece6c2101b09062cd2ee1d33b
alkis <a.kafkis@gmail.com>
2017-07-21 19:36:54 +0300

    Permission request mechanism - semi complete.

--- commons/src/main/java/com/katsuna/commons/KatsunaIntents.java
+++ commons/src/main/java/com/katsuna/commons/KatsunaIntents.java
@@ -6,4 +6,7 @@ package com.katsuna.commons;
 
 public class KatsunaIntents {
     public static final String ACTION_CALLS_VIEW = "com.katsuna.calls.view";
+    public static final String PERMISSIONS_SERVICES = "com.katsuna.services.permissions";
+    public static final String PERMISSIONS_INFOSERVICES = "com.katsuna.infoservices.permissions";
+    public static final String PERMISSIONS_WIDGETS = "com.katsuna.widgets.permissions";
 }
