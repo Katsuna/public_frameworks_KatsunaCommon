a20608555b57d6e59d9a6946c7f011fcbe0e0942
alkis <a.kafkis@gmail.com>
2017-07-23 11:37:14 +0300

    Quick settings color profiles. This closes Katsuna/Design#3 .

--- commons/src/main/java/com/katsuna/commons/utils/ColorAdjuster.java
+++ commons/src/main/java/com/katsuna/commons/utils/ColorAdjuster.java
@@ -1,12 +1,17 @@
 package com.katsuna.commons.utils;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.StateListDrawable;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.katsuna.commons.R;
 import com.katsuna.commons.entities.ColorProfile;
@@ -131,6 +136,67 @@ public class ColorAdjuster {
             textView.setTextColor(black87ResId);
         }
 
+    }
+
+    public static void applyColor(Context context, ToggleButton button, ColorProfile profile) {
+
+        int color1 = ColorCalc.getColor(context, ColorProfileKey.ACCENT1_COLOR, profile);
+        int color2 = ColorCalc.getColor(context, ColorProfileKey.ACCENT2_COLOR, profile);
+
+        StateListDrawable stateListDrawable = (StateListDrawable) button.getBackground();
+        LayerDrawable enabledLayer = (LayerDrawable) getCheckedState(stateListDrawable);
+        GradientDrawable mainBg = (GradientDrawable) enabledLayer.getDrawable(0);
+        mainBg.setColor(color1);
+
+        GradientDrawable bar = (GradientDrawable) enabledLayer.getDrawable(1);
+        bar.setColor(color2);
+
+        int grey600 = ContextCompat.getColor(context, R.color.common_grey600);
+        Drawable[] drawables = button.getCompoundDrawables();
+
+        if (profile == ColorProfile.CONTRAST) {
+            button.setTextColor(ContextCompat.getColorStateList(context,
+                    R.color.common_toggle_contrast_text_color));
+
+            if (drawables[0] != null) {
+                if (drawables[0] instanceof StateListDrawable) {
+                    Drawable checkedDrawable = getCheckedState((StateListDrawable)drawables[0]);
+                    DrawUtils.setColor(checkedDrawable, color2);
+                }
+            }
+        } else {
+            button.setTextColor(grey600);
+            if (drawables[0] instanceof StateListDrawable) {
+                Drawable checkedDrawable = getCheckedState((StateListDrawable)drawables[0]);
+                DrawUtils.setColor(checkedDrawable, grey600);
+            }
+        }
+    }
+
+    private static Drawable getCheckedState(StateListDrawable input) {
+        Drawable output = null;
+
+        final DrawableContainer.DrawableContainerState containerState =
+                (DrawableContainer.DrawableContainerState) input.getConstantState();
+        final Drawable[] children = containerState.getChildren();
+        if (children.length > 0) {
+            output = children[0];
+        }
+
+        return output;
+    }
+
+    private static StateListDrawable makeSelector(Drawable drawable, int checkedColor, int color) {
+        Drawable checkedDrawable =  drawable.getConstantState().newDrawable();
+        checkedDrawable.setColorFilter(checkedColor, PorterDuff.Mode.SRC_ATOP);
+
+        Drawable defaultDrawable =  drawable.getConstantState().newDrawable();
+        defaultDrawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+
+        StateListDrawable res = new StateListDrawable();
+        res.addState(new int[]{android.R.attr.state_checked}, checkedDrawable);
+        res.addState(new int[]{}, defaultDrawable);
+        return res;
     }
 
 }
