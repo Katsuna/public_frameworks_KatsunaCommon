e8fdfa19136ca39c8d8b354ec348591bf5ec1f3c
alkis <a.kafkis@gmail.com>
2017-09-13 21:36:23 +0300

    Permanently denied permissions handling . This closes Katsuna/packages_apps_KatsunaContacts#7.

---
+++ commons/src/main/java/com/katsuna/commons/utils/PermissionUtils.java
@@ -0,0 +1,20 @@
+package com.katsuna.commons.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+
+public class PermissionUtils {
+
+    public static void goToPermissionSettings(Context context) {
+        Intent i = new Intent();
+        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        i.setData(Uri.parse("package:" + context.getPackageName()));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        context.startActivity(i);
+    }
+}
