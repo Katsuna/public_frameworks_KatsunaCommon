2019efc59ef4836adabd2e3c6fd512302c951ebb
alkis <a.kafkis@gmail.com>
2017-09-21 19:11:06 +0300

    Contact name display instead of number. This closes Katsuna/packages_apps_KatsunaMessages#6 .

---
+++ commons/src/main/java/com/katsuna/commons/entities/KatsunaConstants.java
@@ -0,0 +1,8 @@
+package com.katsuna.commons.entities;
+
+public class KatsunaConstants {
+
+    public static final String EXTRA_DISPLAY_NAME = "EXTRA_DISPLAY_NAME";
+    public static final String EXTRA_NUMBER = "EXTRA_NUMBER";
+    public static final String MESSAGE = "MESSAGE";
+}
