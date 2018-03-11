210c07d3a36cbe8f041c3405fd81994b326745c6
alkis <a.kafkis@gmail.com>
2018-03-11 19:36:00 +0200

    dev-v2 to dev merge configuration

--- commons/src/main/java/com/katsuna/commons/providers/PreferenceProvider.java
+++ commons/src/main/java/com/katsuna/commons/providers/PreferenceProvider.java
@@ -3,8 +3,7 @@ package com.katsuna.commons.providers;
 import android.net.Uri;
 
 public class PreferenceProvider {
-    // Warning dev-v2 branch specific. DONT MERGE WITH DEV!!!
-    public static final String AUTHORITY = "com.katsuna.staging.services.datastore.providers.PreferenceProvider";
+    public static final String AUTHORITY = "com.katsuna.services.datastore.providers.PreferenceProvider";
     public static final String SCHEME = "content://";
 
     public static final String PREFERENCE = SCHEME + AUTHORITY + "/preference";
