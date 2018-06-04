c1d5d292c8941c23784419c52592ead18915d77c
George G <kreach3r@users.noreply.github.com>
2018-06-04 06:40:46 +0200

    Android.mk: Use AAPT2

--- Android.mk
+++ Android.mk
@@ -1,35 +1,47 @@
 LOCAL_PATH := $(call my-dir)
+# Here is the final static library that apps can link against.
+# Applications that use this library must specify
+#
+#   LOCAL_STATIC_ANDROID_LIBRARIES := \
+        KatsunaCommon \
+#       android-support-v4 \
+#       android-support-v7-appcompat \
+#       android-support-v7-recyclerview \
+#       android-support-v7-cardview \
+#       android-support-v13 \
+#       android-support-design
+#
+# in their makefiles to include the resources and their dependencies in their package.
 include $(CLEAR_VARS)
-
-LOCAL_MANIFEST_FILE := commons/src/main/AndroidManifest.xml
-LOCAL_SRC_FILES := $(call all-java-files-under,commons)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/commons/src/main/res
-LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
-LOCAL_RESOURCE_DIR += frameworks/support/v7/cardview/res
-LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
-LOCAL_RESOURCE_DIR += frameworks/support/design/res
-
-LOCAL_STATIC_JAVA_LIBRARIES += jodatime
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-cardview
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
-LOCAL_STATIC_JAVA_LIBRARIES += fabtransformation
-
-LOCAL_STATIC_JAVA_AAR_LIBRARIES := roundedimageview
-
+LOCAL_USE_AAPT2 := true
 LOCAL_MODULE := KatsunaCommon
-
 LOCAL_SDK_VERSION := $(SUPPORT_CURRENT_SDK_VERSION)
 
-#LOCAL_USE_AAPT2 := true
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --generate-dependencies
-LOCAL_AAPT_FLAGS += --no-version-vectors
+LOCAL_SRC_FILES := $(call all-java-files-under,commons)
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/commons/src/main/res
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-recyclerview \
+    android-support-v7-cardview \
+    android-support-v13 \
+    android-support-design
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    jodatime
+
+LOCAL_STATIC_JAVA_AAR_LIBRARIES += \
+    fabtransformation
+
+LOCAL_MANIFEST_FILE := commons/src/main/AndroidManifest.xml
+
+#LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS += \
+    --no-version-vectors
+
 LOCAL_JAR_EXCLUDE_FILES := none
-#LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
