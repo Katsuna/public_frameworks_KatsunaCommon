b593d14e1c3d3de6ff8ac9d050de73279450896f
alkis <a.kafkis@gmail.com>
2018-07-25 18:49:27 +0300

    Swipe behaviour set to 'full length, open & close'. This closes Katsuna/frameworks_KatsunaCommon#12

--- commons/src/main/java/com/katsuna/commons/controls/KatsunaNavigationView.java
+++ commons/src/main/java/com/katsuna/commons/controls/KatsunaNavigationView.java
@@ -1,9 +1,11 @@
 package com.katsuna.commons.controls;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.util.AttributeSet;
+import android.view.Menu;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -24,6 +26,21 @@ public class KatsunaNavigationView extends FrameLayout {
         super(context, attrs, defStyleAttr);
         inflate(getContext(), R.layout.common_navigation_view, this);
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.KatsunaNavigationView);
+        int menuResId = array.getResourceId(R.styleable.KatsunaNavigationView_customMenu, -1);
+        if (menuResId != -1) {
+            // clear the default one
+            Menu menu = mNavigationView.getMenu();
+            if (menu != null) {
+                menu.clear();
+            }
+
+            // set the custom one
+            mNavigationView.inflateMenu(menuResId);
+        }
+
+        array.recycle();
 
         removeOverScroll();
     }
